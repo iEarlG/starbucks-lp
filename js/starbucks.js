1c8758aff3a850d5eb22c60b5197bdd306e1cf7e
Earl Villapaz <jasonburning16@gmail.com>
2023-03-14 19:20:13 +0800

    first commit

---
+++ js/starbucks.js
@@ -0,0 +1,8 @@
+function imgChanger(anything) { 
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
